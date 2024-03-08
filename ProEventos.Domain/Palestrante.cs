b120d9df77b1023fae076853c94d19098b2e5641
Clayton Rocha <clayton.will08@gmail.com>
2024-03-08 15:52:44 -0300

    Criacao das models

---
+++ ProEventos.Domain/Palestrante.cs
@@ -0,0 +1,14 @@
+namespace ProEventos.Domain
+{
+    public class Palestrante
+    {
+        public int id { get; set; }   
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemURL { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<RedeSocial> RedeSocials { get; set; }
+        public IEnumerable<PalestranteEvento> PalestranteEventos { get; set; }
+    }
+}
